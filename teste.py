6e7c911706cc993f19ded22e40eee7260772f5f7
kcharlesk <charleskulkauski@gmail.com>
2021-12-10 11:14:11 -0300

    Exclusão de arquivos

--- teste.py
+++
@@ -1,8 +0,0 @@
-listagem = ('Caderno', 12.00, 'Lápis de cor', 20.50, 'Borracha', 2.00, 'Cola', 3.50, 'Fichário', 32.90, 'Folhas', 10.90,
-            'Livro', 34.90, 'Glitter', 0.50, 'EVA', 1.00, 'Tintas', 12.00, 'Cartolina', 0.75)
-
-for c in listagem:
-    if listagem[c] == slice:
-        print(f'{listagem} .......... ', end='')
-    elif listagem[c] == float:
-        print(f'{listagem[c]} ..........\n')
